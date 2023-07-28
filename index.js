c8080ede347bae0c9a077220f655362a5f5e4f70
tenzin woeser <tenzinwoeser15@gmail.com>
2023-07-28 01:18:37 -0400

    added cors support

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const { connectToDb } = require("./db");
+const cors = require("cors");
 const appRoutes = require("./routes");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 connectToDb().then(() => {
   app.use("/api", appRoutes);
